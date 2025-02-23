2e07821ea58979d7e50819ad0441831fa716b81c
Bank <blankinfear@gmail.com>
2025-02-24 04:02:30 +0700

    answer

--- Java/basic/ex04.java
+++ Java/basic/ex04.java
@@ -1,9 +1,11 @@
 public class ex04 {
-    function sum(int a, int b) {
+    // ฟังก์ชันที่รับ int และคืนค่า int
+    public int sum(int a, int b) {
         return a + b;
     }
 
-    function sum(String a, String b ,String c{
+    // ฟังก์ชันที่รับ String 3 ตัว และคืนค่า String
+    public String sum(String a, String b, String c) {
         return a + b + c;
-    })
+    }
 }
--- Java/basic/ex05.java
+++ Java/basic/ex05.java
@@ -1,7 +1,11 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class ex05 {
-    ArrayList<String> fruits = new ArrayList<String>("Apple", "Banana", "Cherry");
-    System.out.println(fruits);
+    public static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<String>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Cherry");
+        System.out.println(fruits);
+    }
 }
