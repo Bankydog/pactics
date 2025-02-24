81d50b01c073e04a9f8d590b7b2c6ab44dc9c3f8
Bank <blankinfear@gmail.com>
2025-02-25 03:19:50 +0700

    ok

---
+++ Java/basic/ex06.java
@@ -0,0 +1,43 @@
+public class ex06 {
+
+    public interface Shape {
+        double calculateArea();
+    }
+
+    public static class Rectangle implements Shape {
+        private double width;
+        private double height;
+
+        public Rectangle(double width, double height) {
+            this.width = width;
+            this.height = height;
+        }
+
+        @Override
+        public double calculateArea() {
+            return this.width * this.height;
+        }
+    }
+
+    public static class Circle implements Shape {
+        private double radius;
+        private double PI = 3.14;
+
+        public Circle(double radius, double PI) {
+            this.radius = radius;
+            this.PI = PI;
+        }
+
+        @Override
+        public double calculateArea() {
+            return this.PI * this.radius * this.radius;
+        }
+    }
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(10, 20);
+        Circle circle = new Circle(10, 3.14);
+        System.out.println("result of Rectangle is : " + rectangle.calculateArea());
+        System.out.println("result of Circle is : " + circle.calculateArea());
+    }
+}
